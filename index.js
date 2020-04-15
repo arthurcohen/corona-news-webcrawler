6499679055240fd683bca1e13a9559a5884d80d8
Rodolfo Santos <rodolfosvc@gmail.com>
2020-04-15 18:32:55 -0300

    using 'const' and removing unused variables

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 const axios = require('axios');
-const xmlParser = require('xml2js');
 const sources = require('./src/config/source-config');
 const { Parser } = require('json2csv');
 const newsService = require('./news.service');
-fs = require('fs');
+const fs = require('fs');
 
 async function batch(){
     for(let i = 0; i < sources.length; i++){
         source = sources[i];
-
         const response = await axios.get(source.sitemapUrl);
         const urls = await newsService.getNewsUrlFromSitemap(response.data);
         const allNews = [];
     
-        for (let i = 0; i < 3 /*urls.length*/; i++) {
-            var url = urls[i];
-            var httpResponse = await axios.get(url);
-
+        for (let i = 0; i < 6 /*urls.length*/; i++) {
+            const url = urls[i];
+            const httpResponse = await axios.get(url);
             const news = newsService.getNewsFromHtml(httpResponse.data, source);
             news.url = url;
-    
             allNews.push(news);
-            console.log(`Notícia ${i + 1} do ${source.sourceName}`);
         }
-    
+
         const parser = new Parser();
         const csv = parser.parse(allNews);
     
@@ -34,6 +29,5 @@ async function batch(){
         });
     }
 }
-
 
 batch();

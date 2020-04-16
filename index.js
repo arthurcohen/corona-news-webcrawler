63949048e11f10dc18e3fc4939eac104eb7fa488
Arthur Cohen <laarthur.cohen@MacBook-Pro-de-Arthur.local>
2020-04-16 14:27:00 -0300

    limiting number of news that are fetched

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ async function batch(){
         const response = await axios.get(source.sitemapUrl);
         const urls = await newsService.getNewsUrlFromSitemap(response.data);
 
-        for (const url of urls) {
+        for (const url of urls.splice(0, 2)) {
             const httpResponse = await axios.get(url);
             const news = newsService.getNewsFromHtml(httpResponse.data, source);
             news.url = url;

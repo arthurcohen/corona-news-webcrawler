a94ad8aa40cfa9b5e27eb362e79978fe9118d4cc
Arthur Cohen <laarthur.cohen@MacBook-Pro-de-Arthur.local>
2020-04-27 13:27:03 -0300

    source file for folha sp

---
+++ src/new-sources/folhasp.ts
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-unused-vars
+import { Source } from '../interfaces/source';
+
+const source: Source = {
+  sourceName: 'Folha de SP',
+  sitemapUrl: 'https://www1.folha.uol.com.br/folha-online-sitemap.xml',
+  profile: {
+    titlePattern: {
+      pattern: 'meta[property="og:title"]',
+      isProp: true
+    },
+    imagePattern: {
+      pattern: 'meta[property="og:image"]',
+      isProp: true
+    },
+    publicationDatePattern: {
+      pattern: 'meta[property="article:published_time"]',
+      isProp: true
+    }
+  }
+};
+
+export default source;

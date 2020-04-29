152b132deb21b112a27b0bc8c6c9f06007308e56
fernandolucio <fernandolucio8@gmail.com>
2020-04-29 14:50:54 -0300

    Update code

---
+++ src/new-sources/estado-de-minas.ts
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-unused-vars
+import { Source } from '../interfaces/source';
+
+const source: Source = {
+  sourceName: 'Jornal Estado de Minas',
+  sitemapUrl: 'https://www.em.com.br/sitemapnews.xml',
+  language: 'pt-br',
+  date: '',
+  profile: {
+    titlePattern: {
+      pattern: 'meta[property="og:title"]',
+      isProp: true
+    },
+    imagePattern: {
+      pattern: 'meta[property="og:image"]',
+      isProp: true
+    },
+    publicationDatePattern: {
+      pattern: '',
+      isProp: true
+    }
+  }
+};
+
+export default source;

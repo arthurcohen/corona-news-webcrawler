3074093508c46228d21bcaa58f62c8c46136b08e
Roberto Oliveira da Silba Filho <laroberto.oliveira@Robertos-MacBook-Pro.local>
2020-04-30 15:52:28 -0300

    Add question in cmd

--- index.ts
+++ index.ts
@@ -1,3 +1,55 @@
 import batch from './src/index';
+import sourceFactory from './src/config/source-config';
+var inquirer = require('inquirer');
 
-batch();
+const chooseLanguage = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'language',
+        message: 'Choose language.',
+        choices: ['pt-br', 'en-us', 'All'],
+        filter: function(val) {
+          return val.toLowerCase();
+        }
+      }
+    ])
+    .then(answers => {
+      chooseFonts(answers.language);
+    });
+};
+
+const chooseFonts = async (language = 'pt-br') => {
+  let sources = await sourceFactory;
+  sources = sources.filter(
+    (s) => s.language.toLowerCase() === language.toLowerCase() || language === 'all'
+  );
+  inquirer
+    .prompt([
+      {
+        type: 'checkbox',
+        message: 'Select sources.',
+        name: 'fonts',
+        choices: [...sources.map((item) => { return { name: item.sourceName }; }), { name: 'All' }],
+        validate: function(answer) {
+          if (answer.length < 1) {
+            return 'You must choose at least one topping.';
+          }
+          return true;
+        }
+      }
+    ])
+    .then(answers => {
+      let sourcesArray = [];
+      answers.fonts.filter(answer => {
+        sources.filter(item => {
+          if (answer === item.sourceName) sourcesArray.push(item);
+        });
+      });
+      if (answers.fonts.find(item => (item === 'All'))) sourcesArray = sources;
+      batch(sourcesArray);
+    });
+};
+
+chooseLanguage();

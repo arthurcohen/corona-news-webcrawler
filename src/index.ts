3c45c48af4d3033e7623da57e34d54737c7547ad
fernandolucio <fernandolucio8@gmail.com>
2020-05-27 16:43:13 -0300

    Adjusts

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,10 @@ import dataService from './services/data.service';
 // eslint-disable-next-line no-unused-vars
 import { Source } from './interfaces/source';
 
-async function batch(sourcesArray : Source[], language:string) {
+async function batch(sourcesArray: Source[], language: string) {
   process.stdout.write('fetching news from');
   dataService.configuraDataService(language);
+  const opn = require('open');
 
   for (const source of sourcesArray) {
     process.stdout.write(`\n${source.sourceName}: `);
@@ -19,7 +20,10 @@ async function batch(sourcesArray : Source[], language:string) {
       continue;
     }
 
-    let urls = await newsService.getNewsUrlFromSitemap(response.data, source.filterUrlset);
+    let urls = await newsService.getNewsUrlFromSitemap(
+      response.data,
+      source.filterUrlset
+    );
     urls = dataService.filterNews(urls);
 
     for (const url of urls) {
@@ -40,7 +44,9 @@ async function batch(sourcesArray : Source[], language:string) {
         process.stdout.write('.');
       }
     }
+
     dataService.saveFile();
+    opn('./files/index.html');
     process.stdout.write(` (${new Date().getTime() - startTime} ms)`);
   }
 }
